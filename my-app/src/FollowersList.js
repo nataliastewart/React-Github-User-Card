d6421518c519afb853a7dd4310f1ffd6dd1fe1f3
nataliastewart <nataliastewart@users.noreply.github.com>
2020-04-08 22:23:21 -0400

    Styled components added

--- my-app/src/FollowersList.js
+++ my-app/src/FollowersList.js
@@ -1,15 +1,25 @@
 import React from "react";
-import UserCard from "./UserCard";
+import styled from "styled-components";
+
+const Container = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-around;
+  flex-wrap: wrap;
+`;
+
+// const cardImg = styled.img`
+//   width: 30px;
+//   border-radius: 30px;
+// `;
 
 export default function FollowersList(props) {
   return (
-    <div>
-      <h4>{props.person.login}</h4>
-      <img src={props.person.avatar_url} alt="follower"></img>
-    </div>
+    <Container>
+      <div>
+        <h4>{props.person.login}</h4>
+        <img src={props.person.avatar_url} alt="follower"></img>
+      </div>
+    </Container>
   );
-}
-
-{
-  /* // <UserCard key={item.id} item={item} /> */
 }
